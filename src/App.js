78805a8d7570f2c3778abd62bbf25f21b88d8e1d
akile99 <akile99@gmail.com>
2021-10-13 15:22:40 -0400

    added component

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
 import './App.css';
-import DropdownButton from 'react-bootstrap/DropdownButton'
-import Dropdown from 'react-bootstrap/Dropdown'
+import { Menu } from './components/menu/menu.component'
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
       <h1>Meal Now</h1>
-      <Dropdown>
-        <Dropdown.Toggle variant="success" id="dropdown-basic">
-          Dropdown Button
-        </Dropdown.Toggle>
-
-        <Dropdown.Menu>
-          <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-          <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-          <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
+      <Menu />
     </div>
   );
 }
